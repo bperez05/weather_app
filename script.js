b5e25000310fde508efd53cb0a7e1a5486a9253c
bperez05 <perez.brandon55@yahoo.com>
2021-10-05 00:40:15 -0500

    cards and form box

---
+++ script.js
@@ -0,0 +1,19 @@
+// constant variables
+var apikey = "8b0fec354dc1afc925b5e9e47dc79b82";
+var apiUrl = https://api.openweathermap.org;
+var cityOutput = [];
+
+
+// dom element references
+
+
+
+
+
+function currentWeather (city, weather) {
+    
+    var temp = weather.temp
+    var wind = weather.windspeed
+
+   
+}
