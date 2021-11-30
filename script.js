6bbe6075f0e3c736c72c67f479b1a0d3cb32320d
bperez05 <perez.brandon55@yahoo.com>
2021-11-30 02:37:51 -0600

    added api params

--- script.js
+++ script.js
@@ -1,19 +1,87 @@
 // constant variables
-var apikey = "8b0fec354dc1afc925b5e9e47dc79b82";
-var apiUrl = https://api.openweathermap.org;
+var apikey = "66362f6b0e9db023ad53305c124d1c5c";
+//var apiUrl = https://api.openweathermap.org;
 var cityOutput = [];
+var cityInpEL = document.querySelector("#city");
+var citySearch = document.querySelector("#city_search");
+var cityLiEl = document.querySelector("#city-li");
+var citySearch;
+var uvIndexEL;
+var currentWeatherEL;
+var currentCity;
+var weatherContEl = document.querySelector("#weather-cont");
+var forecastContEl = document.querySelector("#forecast-cont");
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+// displays city name
+    var city = cityInputEl.value.trim();
+    citySearch = cityInputEl.value.trim();
+
+    if (city) {
+        getLatLon(city);
+        cityList.unshift(city);
+        storePreviousCity();
+        renderPreviousCity();
+        cityInpEl.value = "";
+    } else {
+        alert("Please enter a city name");
+    }
+};
+var getWeather = function(lat, lon) {
+    var apiKey = '66362f6b0e9db023ad53305c124d1c5c'
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&appid=" + apiKey + "&units=imperial";
+
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    currentForecast = data.current;
+                    weeklyForecast = data.daily;
+                    displayWeather(weeklyForecast);
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function(error) {
+            alert('Unable to connect to database');
+        });
+};
+var getLatLon = function(city) {
+    var apiKey = "66362f6b0e9db023ad53305c124d1c5c"
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey + '&units=imperial';
+
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+               
+                response.json().then(function(data) {
+                    
+                    currentCity = data.city.name
+                        
+                    getWeather(data.city.coord.lat, data.city.coord.lon);
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+                //return lat and lon or should i jsut save the api response as an object and only use the lat lon info?
+            }
+        }).catch(function(error) {
+            alert('Unable to data');
+        });
+}
 
 
-// dom element references
+    // dom element references
 
 
 
 
 
-function currentWeather (city, weather) {
+//function currentWeather (city, weather) {
     
-    var temp = weather.temp
-    var wind = weather.windspeed
+    //var temp = weather.temp
+    //var wind = weather.windspeed
 
    
-}
+//}
